0199b4a4a329bee0e4facbcc64555f25fb45b17a
Dirt Deodara <dirtdeodara@lithia.com>
2020-11-17 14:47:37 -0800

    more dog stuf

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,7 +5,7 @@ import drivewayLogo from '../../icons/driveway-logo.svg';
 const Header = () => {
   const [tagLine, setTagLine] = useState("BRING IT HOME") 
   const cycleTagLine = () => {
-    const array = ["BRING IT HOME", "HORN GO BEEP", "CAR GO VROOM", "DITCH THE DEALERSHIP", "CONVENIENCE DELIVERED"]; 
+    const array = ["BRING IT HOME", "HORN GO BEEP", "CAR GO VROOOOM", "DITCH THE DEALERSHIP", "CONVENIENCE DELIVERED", "ME LIKE CARS"]; 
     setInterval(() => {
       setTagLine(array[Math.floor(Math.random() * 5)]);
     }, 20000)
