05de83c20a0523d5e65e24bb72629309348e1a21
Dannie Schumaker <DannieSchumaker@lithia.com>
2020-11-18 00:08:34 -0800

    pass shoutout objects to/from server

--- src/components/Join/Join.js
+++ src/components/Join/Join.js
@@ -4,15 +4,13 @@ import './Join.css';
 
 const Join = () => {
   const [name, setName] = useState('');
-  const [team, setTeam] = useState('');
 
   return(
     <div className="joinOuterContainer">
       <div className="joinInnerContainer">
         <h1 className="heading">Join</h1>
         <div><input placeholder="Name" className="joinInput" type="text" onChange={(event) => setName(event.target.value)}/></div>
-        <div><input placeholder="Team" className="joinInput mt-20" type="text" onChange={(event) => setTeam(event.target.value)}/></div>
-        <Link onClick={(event) => (!name || !team) ? event.preventDefault() : null} to={`/chat?name=${name}&team=${team}`}>
+        <Link onClick={(event) => (!name) ? event.preventDefault() : null} to={`/chat?name=${name}`}>
           <button className="btn mt-20" type="submit">Sign In</button>
         </Link>
       </div>
