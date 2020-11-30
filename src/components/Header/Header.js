8f8ddeae14ab2ad9c0035a07bf6e3390ec531331
Dirt Deodara <dirtdeodara@lithia.com>
2020-11-30 13:49:14 -0800

    added a cleanup method on the cycleTagLine call

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,13 +5,16 @@ import drivewayLogo from '../../icons/driveway-logo.svg';
 const Header = () => {
   const [tagLine, setTagLine] = useState("BRING IT HOME") 
   const cycleTagLine = () => {
-    const array = ["BRING IT HOME", "HORN GO BEEP", "CAR GO VROOOOM", "DITCH THE DEALERSHIP", "CONVENIENCE DELIVERED", "ME LIKE CARS"]; 
+    const array = ["BRING IT HOME", "HORN GO BEEP", "CAR GO VROOOOM", "DITCH THE DEALERSHIP", "CONVENIENCE DELIVERED", "ME LIKE CARS", "DON'T WANNA CARVANA?"]; 
     setInterval(() => {
-      setTagLine(array[Math.floor(Math.random() * 5)]);
+      setTagLine(array[Math.floor(Math.random() * 7)]);
     }, 20000)
   }
   useEffect(() => {
     cycleTagLine();
+    return () => {
+      setTagLine("BRING IT HOME");
+    };
   }, []);
 
   return (
