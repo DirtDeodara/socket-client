8d26320a5b11ed1a731f70925c6e4e21ab39a4bc
Jenna Goldman <jennagoldman@lithia.com>
2020-12-09 18:15:41 -0800

    remove old colorfactory data

--- src/components/Shoutout/shoutoutColors.js
+++ src/components/Shoutout/shoutoutColors.js
@@ -1,40 +1,4 @@
 const colorFactory = {
-    // blue: {
-    //   mainBackground: "#1cb7eb",
-    //   text: "white",
-    //   commentBackground: "#0192D2",
-    //   accent: "#ffed49"
-    // },
-    // green: {
-    //   mainBackground: "#33FF66",
-    //   text: "#333333",
-    //   commentBackground: "#31c957",
-    //   accent: "#ad0dc2"
-    // },
-    // purple: {
-    //   mainBackground: "#CC66FF",
-    //   text: "white",
-    //   commentBackground: "#a428e2",
-    //   accent: "#265bce"
-    // },
-    // orange: {
-    //   mainBackground: "#FF9933",
-    //   text: "#333333",
-    //   commentBackground: "#e27c16",
-    //   accent: "#058dcc"
-    // },
-    // pink: {
-    //   mainBackground: "#FF66CC",
-    //   text: "white",
-    //   commentBackground: "#db1098",
-    //   accent: "#059200"
-    // },
-    // yellow: {
-    //   mainBackground: "#ffed49",
-    //   text: "#333333",
-    //   commentBackground: "#e4d01e",
-    //   accent: "#7c13b1"
-    // }
     red: {
       mainBackground: "#C13A5F",
       text: "white",
@@ -74,40 +38,3 @@ const colorFactory = {
   };
 
   export default colorFactory;
-
-  // purple: {
-  //   mainBackground: "#751A33",
-  //   text: "white",
-  //   commentBackground: "#1A3375",
-  //   accent: "#33751A"
-  // },
-  // darkOrange: {
-  //   mainBackground: "#B34233",
-  //   text: "white",
-  //   commentBackground: "#4233B3",
-  //   accent: "#33B342"
-  // },
-  // orange: {
-  //   mainBackground: "#D28F33",
-  //   text: "#333333",
-  //   commentBackground: "#D23F33",
-  //   accent: "#C6D233"
-  // },
-  // yellow: {
-  //   mainBackground: "#D4B95E",
-  //   text: "#333333",
-  //   commentBackground: "#CC6741",
-  //   accent: "#809f2b"
-  // },
-  // lightBlue: {
-  //   mainBackground: "#4EA2A2",
-  //   text: "#333333",
-  //   commentBackground: "#7C227C",
-  //   accent: "#C9C927"
-  // },
-  // blue: {
-  //   mainBackground: "#1A8693",
-  //   text: "white",
-  //   commentBackground: "#A2A24E",
-  //   accent: "#7C227C"
-  // }
